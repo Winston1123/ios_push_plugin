c5c66a662102b3bb0e800838323afbc187330661
Mosquito1123 <zhangwentong1123@icloud.com>
2025-10-31 19:55:29 +0800

    MOD:修改原生代码

--- ios/Classes/FlutterApnsSerialization.swift
+++ ios/Classes/FlutterApnsSerialization.swift
@@ -6,7 +6,7 @@ class FlutterApnsSerialization {
         var data: [String: Any] = [:]
         var notification: [String: Any] = [:]
         var notificationIOS: [String: Any] = [:]
-
+        
         // message.data
         for (key, value) in userInfo {
             if let keyString = key as? String {
@@ -15,74 +15,74 @@ class FlutterApnsSerialization {
                     message["messageId"] = value
                     continue
                 }
-
+                
                 // message.messageType
                 if keyString == "message_type" {
                     message["messageType"] = value
                     continue
                 }
-
+                
                 // message.collapseKey
                 if keyString == "collapse_key" {
                     message["collapseKey"] = value
                     continue
                 }
-
+                
                 // message.from
                 if keyString == "from" {
                     message["from"] = value
                     continue
                 }
-
+                
                 // message.sentTime
                 if keyString == "google.c.a.ts" {
                     message["sentTime"] = value
                     continue
                 }
-
+                
                 // message.to
                 if keyString == "to" || keyString == "google.to" {
                     message["to"] = value
                     continue
                 }
-
+                
                 // build data dict from remaining keys but skip keys that shouldn't be included in data
                 if keyString == "aps" || keyString.hasPrefix("gcm.") || keyString.hasPrefix("google.") {
                     continue
                 }
-
+                
                 // message.apple.imageUrl
                 if keyString == "fcm_options", let options = value as? [String: Any], let image = options["image"] as? String {
                     notificationIOS["imageUrl"] = image
                     continue
                 }
-
+                
                 data[keyString] = value
             }
         }
         message["data"] = data
-
+        
         if let apsDict = userInfo["aps"] as? [String: Any] {
             // message.category
             if let category = apsDict["category"] as? String {
                 message["category"] = category
             }
-
+            
             // message.threadId
             if let threadId = apsDict["thread-id"] as? String {
                 message["threadId"] = threadId
             }
-
+            
             // message.contentAvailable
             if let contentAvailable = apsDict["content-available"] as? Int {
                 message["contentAvailable"] = contentAvailable != 0
             }
-
+            
             // message.mutableContent
             if let mutableContent = apsDict["mutable-content"] as? Int {
                 message["mutableContent"] = mutableContent != 0
             }
-
+            
             // message.notification.*
             if let alert = apsDict["alert"] {
                 if let alertDict = alert as? [String: Any] {
@@ -90,50 +90,50 @@ class FlutterApnsSerialization {
                     if let title = alertDict["title"] as? String {
                         notification["title"] = title
                     }
-
+                    
                     // message.notification.titleLocKey
                     if let titleLocKey = alertDict["title-loc-key"] as? String {
                         notification["titleLocKey"] = titleLocKey
                     }
-
+                    
                     // message.notification.titleLocArgs
                     if let titleLocArgs = alertDict["title-loc-args"] as? [String] {
                         notification["titleLocArgs"] = titleLocArgs
                     }
-
+                    
                     // message.notification.body
                     if let body = alertDict["body"] as? String {
                         notification["body"] = body
                     }
-
+                    
                     // message.notification.bodyLocKey
                     if let bodyLocKey = alertDict["loc-key"] as? String {
                         notification["bodyLocKey"] = bodyLocKey
                     }
-
+                    
                     // message.notification.bodyLocArgs
                     if let bodyLocArgs = alertDict["loc-args"] as? [String] {
                         notification["bodyLocArgs"] = bodyLocArgs
                     }
-
+                    
                     // Apple only
                     // message.notification.apple.subtitle
                     if let subtitle = alertDict["subtitle"] as? String {
                         notificationIOS["subtitle"] = subtitle
                     }
-
+                    
                     // Apple only
                     // message.notification.apple.subtitleLocKey
                     if let subtitleLocKey = alertDict["subtitle-loc-key"] as? String {
                         notificationIOS["subtitleLocKey"] = subtitleLocKey
                     }
-
+                    
                     // Apple only
                     // message.notification.apple.subtitleLocArgs
                     if let subtitleLocArgs = alertDict["subtitle-loc-args"] as? [String] {
                         notificationIOS["subtitleLocArgs"] = subtitleLocArgs
                     }
-
+                    
                     // Apple only
                     // message.notification.apple.badge
                     if let badge = alertDict["badge"] {
@@ -143,7 +143,7 @@ class FlutterApnsSerialization {
                 notification["apple"] = notificationIOS
                 message["notification"] = notification
             }
-
+            
             // message.notification.apple.sound
             if let sound = apsDict["sound"] {
                 if let soundString = sound as? String {
@@ -158,24 +158,24 @@ class FlutterApnsSerialization {
                     if let name = soundDict["name"] as? String {
                         notificationIOSSound["name"] = name
                     }
-
+                    
                     // message.notification.apple.sound.critical Boolean
                     if let critical = soundDict["critical"] as? Bool {
                         notificationIOSSound["critical"] = critical
                     }
-
+                    
                     // message.notification.apple.sound.volume Number
                     if let volume = soundDict["volume"] as? Double {
                         notificationIOSSound["volume"] = volume
                     }
-
+                    
                     notificationIOS["sound"] = notificationIOSSound
                 }
                 notification["apple"] = notificationIOS
                 message["notification"] = notification
             }
         }
-
+        
         return message
     }
 }
--- ios/Classes/IosPushPlugin.swift
+++ ios/Classes/IosPushPlugin.swift
@@ -2,137 +2,117 @@ import Flutter
 import UIKit
 import UserNotifications
 
-public class IosPushPlugin: NSObject,  FlutterPlugin,UNUserNotificationCenterDelegate {
-   var channel: FlutterMethodChannel?
-      var result:FlutterResult?
-      var resumingFromBackground = false
-      var regId:String?
-
-      let manufacturer = "APPLE"
-
-      init(channel: FlutterMethodChannel) {
-          self.channel = channel
-          super.init()
-      }
-
-      public static func register(with registrar: FlutterPluginRegistrar) {
-          let channel = FlutterMethodChannel(name: "ios_push_plugin", binaryMessenger: registrar.messenger())
-          let instance = IosPushPlugin(channel: channel)
-          registrar.addApplicationDelegate(instance)
-          registrar.addMethodCallDelegate(instance, channel: channel)
-      }
-
-      public func handle(_ call: FlutterMethodCall, result: @escaping FlutterResult) {
-          switch call.method {
-          case "getPlatformVersion":
-              result("iOS " + UIDevice.current.systemVersion)
-          case "initPush":
-              initPush(result:result)
-          case "getRegId":
-              result(regId)
-          case "getManufacturer":
-              result(manufacturer)
-          case "enableLog":
-              if let isEnabled = call.arguments as? Bool {
-                  Logger.isEnabled = isEnabled
-              }
-          default:
-              result(FlutterMethodNotImplemented)
-          }
-      }
-
-      private func initPush(result: @escaping FlutterResult) {
-          self.result = result;
-
-          guard UNUserNotificationCenter.current().delegate != nil else {
-              print("Init Fail delegate is nil")
-              result(FlutterError(code: "10086", message: "Init Fail delegate is nil", details: nil))
-              self.result = nil;
-              return
-          }
-
-          // 注册远程通知
-          UIApplication.shared.registerForRemoteNotifications()
-
-      }
-
-      public func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [AnyHashable : Any] = [:]) -> Bool {
-
-          UNUserNotificationCenter.current().delegate = self
-          return true
-      }
-
-      public func applicationDidEnterBackground(_ application: UIApplication) {
-          resumingFromBackground = true
-      }
-
-      public func applicationDidBecomeActive(_ application: UIApplication) {
-          resumingFromBackground = false
-          UIApplication.shared.applicationIconBadgeNumber = -1;
-      }
-
-      public func application(_ application: UIApplication, didRegisterForRemoteNotificationsWithDeviceToken deviceToken: Data) {
-
-          let deviceTokenString = deviceToken.map { String(format: "%02.2hhx", $0) }.joined()
-          self.regId = deviceTokenString
-          Logger.log("onResister Success")
-          result?(["regId":regId,"manufacturer":"APPLE"])
-      }
-
-
-      public func application(_ application: UIApplication, didReceiveRemoteNotification userInfo: [AnyHashable : Any], fetchCompletionHandler completionHandler: @escaping (UIBackgroundFetchResult) -> Void) -> Bool {
-          let userInfo = FlutterApnsSerialization.remoteMessageUserInfoToDict(userInfo)
-
-          onMessage(userInfo: userInfo)
-          completionHandler(.noData)
-          return true
-      }
-
-      public func userNotificationCenter(_ center: UNUserNotificationCenter, willPresent notification: UNNotification, withCompletionHandler completionHandler: @escaping (UNNotificationPresentationOptions) -> Void) {
-          let userInfo = notification.request.content.userInfo
-
-          guard userInfo["aps"] != nil else {
-              return
-          }
-
-          _ = FlutterApnsSerialization.remoteMessageUserInfoToDict(userInfo)
-          completionHandler([.alert, .sound])
-      }
-
-      public func userNotificationCenter(_ center: UNUserNotificationCenter, didReceive response: UNNotificationResponse, withCompletionHandler completionHandler: @escaping () -> Void) {
-          var userInfo = response.notification.request.content.userInfo
-          guard userInfo["aps"] != nil else {
-              return
-          }
-
-          userInfo["actionIdentifier"] = response.actionIdentifier
-          _ = FlutterApnsSerialization.remoteMessageUserInfoToDict(userInfo)
-
-          onMessage(userInfo: userInfo)
-          completionHandler()
-      }
-
-
-      private func onMessage(userInfo: [AnyHashable: Any]) {
-          Logger.log("onMessage")
-          channel?.invokeMethod("onNotificationClick", arguments: ((userInfo["aps"] as? [String: Any])?["attributes"] as? [String: Any])?["data"] as? String ?? nil)
-      }
-
-  }
-  extension UNNotificationCategoryOptions {
-      static let stringToValue: [String: UNNotificationCategoryOptions] = {
-          var r: [String: UNNotificationCategoryOptions] = [:]
-          r["UNNotificationCategoryOptions.customDismissAction"] = .customDismissAction
-          r["UNNotificationCategoryOptions.allowInCarPlay"] = .allowInCarPlay
-          if #available(iOS 11.0, *) {
-              r["UNNotificationCategoryOptions.hiddenPreviewsShowTitle"] = .hiddenPreviewsShowTitle
-          }
-          if #available(iOS 11.0, *) {
-              r["UNNotificationCategoryOptions.hiddenPreviewsShowSubtitle"] = .hiddenPreviewsShowSubtitle
-          }
-          if #available(iOS 13.0, *) {
-              r["UNNotificationCategoryOptions.allowAnnouncement"] = .allowAnnouncement
-          }
-          return r
-      }()
+public class IosPushPlugin: NSObject, FlutterPlugin, UNUserNotificationCenterDelegate {
+    private var channel: FlutterMethodChannel
+    private var regId: String?
+    private var notificationCallback: ((String?) -> Void)?
+    private let manufacturer = "APPLE"
+    
+    init(channel: FlutterMethodChannel) {
+        self.channel = channel
+        super.init()
+    }
+    
+    public static func register(with registrar: FlutterPluginRegistrar) {
+        let channel = FlutterMethodChannel(name: "ios_push_plugin", binaryMessenger: registrar.messenger())
+        let instance = IosPushPlugin(channel: channel)
+        registrar.addApplicationDelegate(instance)
+        registrar.addMethodCallDelegate(instance, channel: channel)
+    }
+    
+    public func handle(_ call: FlutterMethodCall, result: @escaping FlutterResult) {
+        switch call.method {
+        case "getPlatformVersion":
+            result("iOS " + UIDevice.current.systemVersion)
+        case "initPush":
+            initPush(result: result)
+        case "getRegId":
+            result(regId)
+        case "getManufacturer":
+            result(manufacturer)
+        case "enableLog":
+            if let isEnabled = call.arguments as? Bool {
+                Logger.isEnabled = isEnabled
+            }
+            result(nil)
+        case "setNotificationClickListener":
+            notificationCallback = call.arguments as? ((String?) -> Void)
+            result(nil)
+        default:
+            result(FlutterMethodNotImplemented)
+        }
+    }
+    
+    private func initPush(result: @escaping FlutterResult) {
+        UNUserNotificationCenter.current().delegate = self
+        UNUserNotificationCenter.current().requestAuthorization(options: [.alert, .sound, .badge]) { granted, error in
+            DispatchQueue.main.async {
+                if granted {
+                    UIApplication.shared.registerForRemoteNotifications()
+                    result("APNs Init Success")
+                } else {
+                    result(FlutterError(code: "10086", message: "APNs permission denied", details: nil))
+                }
+            }
+        }
+    }
+    
+    // MARK: - AppDelegate Hooks
+    
+    public func application(_ application: UIApplication, didRegisterForRemoteNotificationsWithDeviceToken deviceToken: Data) {
+        let token = deviceToken.map { String(format: "%02.2hhx", $0) }.joined()
+        self.regId = token
+        Logger.log("APNs register success: \(token)")
+        
+        // 通知 Flutter
+        channel.invokeMethod("onRegId", arguments: ["regId": token, "manufacturer": manufacturer])
+    }
+    
+    public func application(_ application: UIApplication, didFailToRegisterForRemoteNotificationsWithError error: Error) {
+        Logger.log("APNs register failed: \(error)")
+        channel.invokeMethod("onRegId", arguments: ["error": error.localizedDescription])
+    }
+    
+    // MARK: - Notification Callbacks
+    
+    public func userNotificationCenter(_ center: UNUserNotificationCenter,
+                                       willPresent notification: UNNotification,
+                                       withCompletionHandler completionHandler: @escaping (UNNotificationPresentationOptions) -> Void) {
+        let userInfo = notification.request.content.userInfo
+        completionHandler([.alert, .sound])
+        onMessage(userInfo: userInfo)
+    }
+    
+    public func userNotificationCenter(_ center: UNUserNotificationCenter,
+                                       didReceive response: UNNotificationResponse,
+                                       withCompletionHandler completionHandler: @escaping () -> Void) {
+        let userInfo = response.notification.request.content.userInfo
+        onMessage(userInfo: userInfo)
+        completionHandler()
+    }
+    
+    private func onMessage(userInfo: [AnyHashable: Any]) {
+        Logger.log("Notification received: \(userInfo)")
+        // 通过回调发送到 Flutter
+        let data = (userInfo["aps"] as? [String: Any])?["attributes"] as? [String: Any]? ?? nil
+        notificationCallback?(data?["data"] as? String)
+        channel.invokeMethod("onNotificationClick", arguments: data?["data"] as? String)
+    }
+}
+extension UNNotificationCategoryOptions {
+    static let stringToValue: [String: UNNotificationCategoryOptions] = {
+        var r: [String: UNNotificationCategoryOptions] = [:]
+        r["UNNotificationCategoryOptions.customDismissAction"] = .customDismissAction
+        r["UNNotificationCategoryOptions.allowInCarPlay"] = .allowInCarPlay
+        if #available(iOS 11.0, *) {
+            r["UNNotificationCategoryOptions.hiddenPreviewsShowTitle"] = .hiddenPreviewsShowTitle
+        }
+        if #available(iOS 11.0, *) {
+            r["UNNotificationCategoryOptions.hiddenPreviewsShowSubtitle"] = .hiddenPreviewsShowSubtitle
+        }
+        if #available(iOS 13.0, *) {
+            r["UNNotificationCategoryOptions.allowAnnouncement"] = .allowAnnouncement
+        }
+        return r
+    }()
 }
